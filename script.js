3512aafbfa07db21eb70060fe63d84498333f088
Abdelmajid Sabiri <abdlmajidsabiri26@gmail.com>
2024-06-25 22:34:54 +0100

    Add js part to add check and delete task proprieties

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ function addTask(){
     }
     inputBox.value = '';
 }
+
+listeContainer.addEventListener("click" , function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
