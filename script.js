634c17e259113bf3f4634a1e883ee43439c451fd
Abdelmajid Sabiri <abdlmajidsabiri26@gmail.com>
2024-06-25 22:57:53 +0100

    Add local storage so we keep seeing the task even after refreshing the page

--- script.js
+++ script.js
@@ -14,13 +14,24 @@ function addTask(){
         li.appendChild(span);
     }
     inputBox.value = '';
+    saveData();
 }
 
 listeContainer.addEventListener("click" , function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        saveData();
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        saveData();
     }
 }, false);
+
+function saveData(){
+    localStorage.setItem("data" , listeContainer.innerHTML);
+}
+function showTask(){
+    listeContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
